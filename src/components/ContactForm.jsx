8c61972af305fa84fe71a7d0469aaee6d3dd007e
Martin <mwong179@mylangara.ca>
2022-03-14 03:54:16 -0700

    Website completed close #7 close #8 close #10

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,11 +1,18 @@
+import "../styles/components/contactForm.css";
 
 function ContactForm() {
     return (
-        <div>
-            <h2>Feel free to reach out to me.</h2>
-            <form>
-
-            </form>
+        <div className="form-container">
+            <section className="form-section">
+                <h2>Feel free to reach out to me!</h2>
+                <form>
+                    <div className="form-space">
+                        <input type="text" placeholder="name"></input>
+                        <input type="text" placeholder="email"></input>
+                    </div>
+                    <textarea placeholder="Enter your message"></textarea>
+                </form>
+            </section>
         </div>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "../styles/components/header.css"
+import Pdf from "../Resume 2022.pdf";
 
 function Header() {
     return (
@@ -9,7 +10,12 @@ function Header() {
                     <li><Link to="/" className="navbar-item">Home</Link></li>
                     <li><Link to="/About" className="navbar-item">About</Link></li>
                     <li><Link to="/Contact" className="navbar-item">Contact</Link></li>
-                    <li><Link to="/Contact" className="navbar-item btn resume">Resume</Link></li>
+                    <li><a 
+                        href={Pdf} 
+                        target="_blank"
+                        rel="noreferrer"
+                        className="navbar-item btn resume">Resume
+                    </a></li>
                 </ul>
             </nav>
         </header>
