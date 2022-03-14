245206caa3b2511e6b33108c300edbc3da025ca4
Martin <mwong179@mylangara.ca>
2022-03-14 02:34:10 -0700

    Finished project section so close #6.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './styles/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
 
 function App() {
   return (
@@ -8,7 +10,8 @@ function App() {
       <div id="content=wrapper">
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/Contact" element=""></Route>
+          <Route exact path="/About" element={<About />}></Route>
+          <Route exact path="/Contact" element={<Contact />}></Route>
           <Route path="*" element=""></Route>
         </Routes>
       </div>
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 function Intro() {
     return (
         <div className="Intro-container">
-            <p>👋 Hello, my name is,</p>
+            <p id="intro">👋 Hello, my name is,</p>
             <h1>Martin Wong.</h1>
-            <p>I am a passion-driven computer science student studying at Langara College.</p>
-            <Link to="/Contact" className="navbar-item intro-btn contact">Contact me</Link>
+            <p>Passion-driven computer science student studying at Langara College.</p>
+            <Link to="/Contact" className="intro-btn contact-btn">Contact me</Link>
         </div>
     );
 }
